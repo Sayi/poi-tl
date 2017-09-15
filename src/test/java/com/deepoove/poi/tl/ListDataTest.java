f0d9036db4e9ae5c7d00335729a1176d065c6002
Sayi <adasai90@gmail.com>
2017-09-15 11:03:39 +0800

    数据集合替换单一模板testcase

---
+++ src/test/java/com/deepoove/poi/tl/ListDataTest.java
@@ -0,0 +1,55 @@
+package com.deepoove.poi.tl;
+
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.deepoove.poi.XWPFTemplate;
+import com.deepoove.poi.data.NumbericRenderData;
+import com.deepoove.poi.data.PictureRenderData;
+import com.deepoove.poi.data.RenderData;
+import com.deepoove.poi.data.TableRenderData;
+import com.deepoove.poi.data.TextRenderData;
+import com.deepoove.poi.tl.mypolicy.ListDataRenderPolicy;
+
+public class ListDataTest {
+    
+    @SuppressWarnings("serial")
+    @Test
+    public void testListData() throws Exception {
+        
+        Map<String, Object> datas = new HashMap<String, Object>(){{
+            
+            final List<Object> list = new ArrayList<Object>(){{
+                add("ver 0.0.3");
+                add(new PictureRenderData(100, 120, "src/test/resources/logo.png"));
+                add(new TextRenderData("9d55b8", "Deeply in love with the things you love, just deepoove."));
+                add("ver 0.0.4");
+                add(new PictureRenderData(100, 120, "src/test/resources/logo.png"));
+            }};
+            List<Object> list2 = new ArrayList<Object>(){{
+                add(list);
+                add(list);
+            }};
+            
+            put("website", list2);
+        }};
+        
+        
+        XWPFTemplate template = XWPFTemplate.compile("src/test/resources/PB.docx");
+        template.registerPolicy("website", new ListDataRenderPolicy());
+        
+        template.render(datas);
+
+        FileOutputStream out = new FileOutputStream("out_PB.docx");
+        template.write(out);
+        out.flush();
+        out.close();
+        template.close();
+    }
+
+}
