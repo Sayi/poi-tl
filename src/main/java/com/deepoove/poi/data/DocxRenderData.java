54e74052efd381ad4c1980d5e196f5123e8a41d9
Sayi <adasai90@gmail.com>
2018-10-23 14:42:35 +0800

    DocxRenderData增加循环一次渲染的构造方法

--- src/main/java/com/deepoove/poi/data/DocxRenderData.java
+++ src/main/java/com/deepoove/poi/data/DocxRenderData.java
@@ -16,6 +16,7 @@
 package com.deepoove.poi.data;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -38,9 +39,26 @@ public class DocxRenderData implements RenderData {
         this.docx = docx;
     }
 
+    /**
+     * 待合并文档可以先被数据集合渲染
+     * 
+     * @param docx
+     * @param dataList 数据列表，列表的大小表示循环的次数
+     */
     public DocxRenderData(File docx, List<?> dataList) {
         this.docx = docx;
         this.dataList = dataList;
+    }
+    
+    /**
+     * 待合并文档可以先被数据渲染
+     * 
+     * @param docx
+     * @param data 数据，循环次数为1
+     */
+    public DocxRenderData(File docx, Object data) {
+        this.docx = docx;
+        this.dataList = Arrays.asList(data);
     }
 
     public File getDocx() {
