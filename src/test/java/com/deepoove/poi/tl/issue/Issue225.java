f1d68998aba06f86a09c3c885194a3672f7fb678
Sayi <adasai90@gmail.com>
2019-05-06 00:46:04 +0800

    #225 junit

---
+++ src/test/java/com/deepoove/poi/tl/issue/Issue225.java
@@ -0,0 +1,38 @@
+package com.deepoove.poi.tl.issue;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.deepoove.poi.XWPFTemplate;
+import com.deepoove.poi.data.DocxRenderData;
+
+public class Issue225 {
+
+	@SuppressWarnings("serial")
+	@Test
+	public void testDocxTemplateRender() throws Exception {
+
+		Map<String, Object> datas = new HashMap<String, Object>() {
+			{
+
+				put("date", "2019-05-06");
+				put("first", new DocxRenderData(new File("src/test/resources/issue/225.docx")));
+
+			}
+		};
+
+		XWPFTemplate template = XWPFTemplate.compile("src/test/resources/issue/merge.docx").render(datas);
+
+		FileOutputStream out = new FileOutputStream("out_225.docx");
+		template.write(out);
+		out.flush();
+		out.close();
+		template.close();
+
+	}
+
+}
