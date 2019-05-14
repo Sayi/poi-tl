6d2a8c4dba14b45116e44579d2312739141fda48
Sayi <adasai90@gmail.com>
2019-05-14 22:23:12 +0800

    #212 暴露异常

--- src/main/java/com/deepoove/poi/el/Dot.java
+++ src/main/java/com/deepoove/poi/el/Dot.java
@@ -82,16 +82,14 @@ public class Dot {
 
 		Field field = FieldFinder.find(objClass, key);
 		if (null == field) {
-			logger.error("Connot find the key:" + key + " from Prefix-Model EL:" + target);
+		    throw new ExpressionEvalException("Connot find the key:" + key + " from Prefix-Model EL:" + target);
 		} else {
 			try {
 				return field.get(obj);
 			} catch (Exception e) {
-				logger.error("Error read the property:" + key + " from " + objClass);
+			    throw new ExpressionEvalException("Error read the property:" + key + " from " + objClass);
 			}
 		}
-		return null;
-
 	}
 
 	public String getEl() {
