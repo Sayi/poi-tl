30d8ecbfe91e7d448b3b3ce5ed677e87634f37ef
Sayi <adasai90@gmail.com>
2019-05-22 23:30:18 +0800

    #215 nullToBlank false

---
+++ src/test/java/com/deepoove/poi/tl/issue/Issues215.java
@@ -0,0 +1,39 @@
+package com.deepoove.poi.tl.issue;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.deepoove.poi.XWPFTemplate;
+import com.deepoove.poi.config.Configure;
+import com.deepoove.poi.data.PictureRenderData;
+import com.deepoove.poi.data.TextRenderData;
+
+public class Issues215 {
+
+	@SuppressWarnings("serial")
+	@Test
+	public void testRepeat() throws IOException {
+		Map<String, Object> datas = new HashMap<String, Object>() {
+			{
+				put("name", "Poi-tl");
+				put("word.tt", "模板引擎");
+				put("time", "2018-06-20");
+				put("what",
+						"Java Word模板引擎： Minimal Microsoft word(docx) templating with {{template}} in Java. It works by expanding tags in a template using values provided in a JavaMap or JavaObject.");
+				put("author", new TextRenderData("000000", "Sayi卅一"));
+				put("introduce", "http://www.deepoove.com");
+				put("portrait", new PictureRenderData(60, 60, "src/test/resources/sayi.png"));
+
+			}
+		};
+
+		Configure config = Configure.newBuilder().supportNullToBlank(false).build();
+		XWPFTemplate template = XWPFTemplate.compile("src/test/resources/template.docx", config).render(datas);
+
+		template.writeToFile("out_template_supportnulltoblank.docx");
+	}
+
+}
