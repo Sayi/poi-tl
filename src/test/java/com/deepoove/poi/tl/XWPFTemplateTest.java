7a67cd0c25116fc02919c1786ee8e938cda4b685
Sayi <adasai90@gmail.com>
2019-05-24 17:34:54 +0800

    #159 支持中文的单元测试

--- src/test/java/com/deepoove/poi/tl/XWPFTemplateTest.java
+++ src/test/java/com/deepoove/poi/tl/XWPFTemplateTest.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.deepoove.poi.config.Configure;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -24,114 +23,94 @@ import com.deepoove.poi.tl.source.MyDataModel;
  */
 public class XWPFTemplateTest {
 
-	RowRenderData header;
-	List<RowRenderData> tableDatas;
+    RowRenderData header;
+    List<RowRenderData> tableDatas;
 
-	@Before
-	public void init() {
-		header = new RowRenderData(Arrays.asList(new TextRenderData("FFFFFF", "Word处理解决方案"),
-				new TextRenderData("FFFFFF", "是否跨平台"), new TextRenderData("FFFFFF", "易用性")), "ff9800");
-		RowRenderData row0 = RowRenderData.build("Poi-tl", "纯Java组件，跨平台", "简单：模板引擎功能，并对POI进行了一些封装");
-		RowRenderData row1 = RowRenderData.build("Apache Poi", "纯Java组件，跨平台", "简单，缺少一些功能的封装");
-		RowRenderData row2 = RowRenderData.build("Freemarker", "XML操作，跨平台", "复杂，需要理解XML结构");
-		RowRenderData row3 = RowRenderData.build("OpenOffice", "需要安装OpenOffice软件", "复杂，需要了解OpenOffice的API");
-		RowRenderData row4 = RowRenderData.build("Jacob、winlib", "Windows平台", "复杂，不推荐使用");
-		tableDatas = Arrays.asList(row0, row1, row2, row3, row4);
-	}
+    @Before
+    public void init() {
+        header = new RowRenderData(
+                Arrays.asList(new TextRenderData("FFFFFF", "Word处理解决方案"),
+                        new TextRenderData("FFFFFF", "是否跨平台"), new TextRenderData("FFFFFF", "易用性")),
+                "ff9800");
+        RowRenderData row0 = RowRenderData.build("Poi-tl", "纯Java组件，跨平台", "简单：模板引擎功能，并对POI进行了一些封装");
+        RowRenderData row1 = RowRenderData.build("Apache Poi", "纯Java组件，跨平台", "简单，缺少一些功能的封装");
+        RowRenderData row2 = RowRenderData.build("Freemarker", "XML操作，跨平台", "复杂，需要理解XML结构");
+        RowRenderData row3 = RowRenderData.build("OpenOffice", "需要安装OpenOffice软件",
+                "复杂，需要了解OpenOffice的API");
+        RowRenderData row4 = RowRenderData.build("Jacob、winlib", "Windows平台", "复杂，不推荐使用");
+        tableDatas = Arrays.asList(row0, row1, row2, row3, row4);
+    }
 
-	@SuppressWarnings("serial")
-	@Test
-	public void testRenderMap() throws Exception {
-		Map<String, Object> datas = new HashMap<String, Object>() {
-			{
-				put("header", "Deeply love what you love.");
-				put("name", "Poi-tl");
-				put("word.tt", "模板引擎");
-				put("time", "2018-06-20");
-				put("what",
-						"Java Word模板引擎： Minimal Microsoft word(docx) templating with {{template}} in Java. It works by expanding tags in a template using values provided in a JavaMap or JavaObject.");
-				put("author", new TextRenderData("000000", "Sayi卅一"));
-				put("introduce", "http://www.deepoove.com");
-				put("portrait", new PictureRenderData(60, 60, "src/test/resources/sayi.png"));
+    @SuppressWarnings("serial")
+    @Test
+    public void testRenderMap() throws Exception {
+        Map<String, Object> datas = new HashMap<String, Object>() {
+            {
+                put("header", "Deeply love what you love.");
+                put("name", "Poi-tl");
+                put("word", "模板引擎");
+                put("time", "2018-06-20");
+                put("what",
+                        "Java Word模板引擎： Minimal Microsoft word(docx) templating with {{template}} in Java. It works by expanding tags in a template using values provided in a JavaMap or JavaObject.");
+                put("author", new TextRenderData("000000", "Sayi卅一"));
+                put("introduce", "http://www.deepoove.com");
+                put("portrait", new PictureRenderData(60, 60, "src/test/resources/sayi.png"));
 
-				put("solution_compare", new MiniTableRenderData(header, tableDatas, MiniTableRenderData.WIDTH_A4_FULL));
+                put("solution_compare", new MiniTableRenderData(header, tableDatas,
+                        MiniTableRenderData.WIDTH_A4_FULL));
 
-				put("feature", new NumbericRenderData(new ArrayList<TextRenderData>() {
-					{
-						add(new TextRenderData("Plug-in grammar, add new grammar by yourself"));
-						add(new TextRenderData(
-								"Supports word text, local pictures, web pictures, table, list, header, footer..."));
-						add(new TextRenderData("Templates, not just templates, but also style templates"));
-					}
-				}));
-			}
-		};
+                put("feature", new NumbericRenderData(new ArrayList<TextRenderData>() {
+                    {
+                        add(new TextRenderData("Plug-in grammar, add new grammar by yourself"));
+                        add(new TextRenderData(
+                                "Supports word text, local pictures, web pictures, table, list, header, footer..."));
+                        add(new TextRenderData(
+                                "Templates, not just templates, but also style templates"));
+                    }
+                }));
+            }
+        };
 
-		XWPFTemplate template = XWPFTemplate.compile("src/test/resources/template.docx").render(datas);
-		;
+        XWPFTemplate template = XWPFTemplate.compile("src/test/resources/template.docx")
+                .render(datas);;
 
-		FileOutputStream out = new FileOutputStream("out_template.docx");
-		template.write(out);
-		out.flush();
-		out.close();
-		template.close();
-	}
+        FileOutputStream out = new FileOutputStream("out_template.docx");
+        template.write(out);
+        out.flush();
+        out.close();
+        template.close();
+    }
 
-	@SuppressWarnings("serial")
-	@Test
-	public void testRenderObject() throws Exception {
-		MyDataModel obj = new MyDataModel();
-		obj.setHeader("Deeply love what you love.");
-		obj.setName("Poi-tl");
-		obj.setWord("模板引擎");
-		obj.setTime("2018-06-20");
-		obj.setWhat("Java Word模板引擎： Minimal Microsoft word(docx) templating with {{template}} in Java. It works by expanding tags in a template using values provided in a JavaMap or JavaObject.");
-		obj.setAuthor("Sayi卅一");
-		obj.setIntroduce("http://www.deepoove.com");
-		obj.setPortrait(new PictureRenderData(60, 60, "src/test/resources/sayi.png"));
-		obj.setSolutionCompare(new MiniTableRenderData(header, tableDatas));
-		obj.setFeature(new NumbericRenderData(new ArrayList<TextRenderData>() {
-			{
-				add(new TextRenderData("Plug-in grammar, add new grammar by yourself"));
-				add(new TextRenderData(
-						"Supports word text, local pictures, web pictures, table, list, header, footer..."));
-				add(new TextRenderData("Templates, not just templates, but also style templates"));
-			}
-		}));
-		
+    @SuppressWarnings("serial")
+    @Test
+    public void testRenderObject() throws Exception {
+        MyDataModel obj = new MyDataModel();
+        obj.setHeader("Deeply love what you love.");
+        obj.setName("Poi-tl");
+        obj.setWord("模板引擎");
+        obj.setTime("2018-06-20");
+        obj.setWhat(
+                "Java Word模板引擎： Minimal Microsoft word(docx) templating with {{template}} in Java. It works by expanding tags in a template using values provided in a JavaMap or JavaObject.");
+        obj.setAuthor("Sayi卅一");
+        obj.setIntroduce("http://www.deepoove.com");
+        obj.setPortrait(new PictureRenderData(60, 60, "src/test/resources/sayi.png"));
+        obj.setSolutionCompare(new MiniTableRenderData(header, tableDatas));
+        obj.setFeature(new NumbericRenderData(new ArrayList<TextRenderData>() {
+            {
+                add(new TextRenderData("Plug-in grammar, add new grammar by yourself"));
+                add(new TextRenderData(
+                        "Supports word text, local pictures, web pictures, table, list, header, footer..."));
+                add(new TextRenderData("Templates, not just templates, but also style templates"));
+            }
+        }));
 
-		XWPFTemplate template = XWPFTemplate.compile("src/test/resources/template.docx").render(obj);
+        XWPFTemplate template = XWPFTemplate.compile("src/test/resources/template.docx")
+                .render(obj);
 
-		FileOutputStream out = new FileOutputStream("out_template_object.docx");
-		template.write(out);
-		template.close();
-		out.flush();
-		out.close();
-	}
-
-
-	/**
-	 * 支持中文变量--测试
-	 */
-	@Test
-	public void poiTemplate() {
-		Configure.ConfigureBuilder builder = Configure.newBuilder();
-		builder.buildGramer("${", "}");
-		XWPFTemplate template = XWPFTemplate.compile("src/test/resources/my_template.docx", builder.build()).render(new HashMap<String, Object>() {{
-			put("title", "模板引擎测试");
-			put("rent", "10000");
-			put("签约时间", "2019年5月13日");//支持中文变量，格式为：${签约时间}
-		}});
-
-		try {
-			FileOutputStream out = new FileOutputStream("my_template_out.docx");
-			template.write(out);
-			out.flush();
-			out.close();
-			template.close();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
-
+        FileOutputStream out = new FileOutputStream("out_template_object.docx");
+        template.write(out);
+        template.close();
+        out.flush();
+        out.close();
+    }
 }
--- src/test/java/com/deepoove/poi/tl/render/ELTemplateTest.java
+++ src/test/java/com/deepoove/poi/tl/render/ELTemplateTest.java
@@ -1,6 +1,8 @@
 package com.deepoove.poi.tl.render;
 
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import org.junit.Before;
@@ -21,6 +23,7 @@ import com.deepoove.poi.tl.el.Detail;
 
 /**
  * 点缀法标签
+ * 
  * @author Sayi
  */
 public class ELTemplateTest {
@@ -61,14 +64,49 @@ public class ELTemplateTest {
         detail.setDesc(desc);
         model.setDetail(detail);
 
-        //poi_tl_mode
+        // poi_tl_mode
         XWPFTemplate template = XWPFTemplate.compile("src/test/resources/dot.docx").render(model);
         template.writeToFile("out_dot.docx");
-        
+
         // spel_mode 部分兼容以前的模式
         Configure config = Configure.newBuilder().setElMode(ELModeEnum.SPEL_MODE).build();
         template = XWPFTemplate.compile("src/test/resources/dot.docx", config).render(model);
         template.writeToFile("out_spel_dot.docx");
+    }
+
+    /**
+     * 支持中文变量
+     * @throws IOException 
+     */
+    @SuppressWarnings("serial")
+    @Test
+    public void testChinese() throws IOException {
+        Configure.ConfigureBuilder builder = Configure.newBuilder();
+        XWPFTemplate template = XWPFTemplate
+                .compile("src/test/resources/chinese.docx", builder.build())
+                .render(new HashMap<String, Object>() {
+                    {
+                        put("作者姓名", "Sayi");
+                        put("作者别名", "卅一");
+                        put("authoravatar",
+                                new PictureRenderData(60, 60, "src/test/resources/sayi.png"));
+                        put("detaildiff", new MiniTableRenderData(header, tableDatas,
+                                MiniTableRenderData.WIDTH_A4_FULL));
+                        put("详情网址", new HyperLinkTextRenderData("http://www.deepoove.com",
+                                "http://www.deepoove.com"));
+                        put("详情", new HashMap<String, Object>() {
+                            {
+                                put("描述", new HashMap<String, String>() {
+                                    {
+                                        put("日期", "2019-05-24");
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+
+        template.writeToFile("out_chinese.docx");
     }
 
 }
