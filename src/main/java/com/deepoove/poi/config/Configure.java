8ee60e182f445f39837a1b1cc30010c4f06fbec1
Sayi <adasai90@gmail.com>
2019-11-15 11:37:51 +0800

    选择一个更贴切方法名作为模板和策略的绑定

--- src/main/java/com/deepoove/poi/config/Configure.java
+++ src/main/java/com/deepoove/poi/config/Configure.java
@@ -228,6 +228,11 @@ public class Configure {
             return this;
         }
 
+        public ConfigureBuilder bind(String tagName, RenderPolicy policy) {
+            config.customPolicy(tagName, policy);
+            return this;
+        }
+
         public Configure build() {
             if (config.elMode == ELMode.SPEL_MODE) {
                 supportGrammerRegexForAll();
