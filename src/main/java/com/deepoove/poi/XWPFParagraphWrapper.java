c25ea10beb11c77171ce0f33985fab134fae6196
Sayi <adasai90@gmail.com>
2019-12-12 16:53:04 +0800

    #331 超链接插入位置问题

--- src/main/java/com/deepoove/poi/XWPFParagraphWrapper.java
+++ src/main/java/com/deepoove/poi/XWPFParagraphWrapper.java
@@ -18,6 +18,8 @@ package com.deepoove.poi;
 import java.lang.reflect.Field;
 import java.util.List;
 
+import javax.xml.namespace.QName;
+
 import org.apache.poi.openxml4j.opc.PackageRelationship;
 import org.apache.poi.xwpf.usermodel.IRunBody;
 import org.apache.poi.xwpf.usermodel.IRunElement;
@@ -25,8 +27,10 @@ import org.apache.poi.xwpf.usermodel.XWPFHyperlinkRun;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRelation;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.xmlbeans.QNameSet;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTHyperlink;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTR;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.impl.CTPImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,6 +43,10 @@ import org.slf4j.LoggerFactory;
 public class XWPFParagraphWrapper {
 
     private static Logger logger = LoggerFactory.getLogger(XWPFParagraphWrapper.class);
+    static final QName HYPER_QNAME = new QName("http://schemas.openxmlformats.org/wordprocessingml/2006/main", "hyperlink");
+    static final QNameSet RUN_QNAME_SET = QNameSet.forArray(new QName[] { HYPER_QNAME,
+            new QName("http://schemas.openxmlformats.org/wordprocessingml/2006/main", "r") });
+
 
     XWPFParagraph paragraph;
 
@@ -71,7 +79,7 @@ public class XWPFParagraphWrapper {
         if (pos >= 0 && pos <= paragraph.getRuns().size()) {
             PackageRelationship relationship = paragraph.getDocument().getPackagePart()
                     .addExternalRelationship(link, XWPFRelation.HYPERLINK.getRelation());
-            CTHyperlink hyperlink = paragraph.getCTP().insertNewHyperlink(pos);
+            CTHyperlink hyperlink = insertNewHyperlink(pos);
             hyperlink.setId(relationship.getId());
             CTR ctr = hyperlink.addNewR();
             XWPFHyperlinkRun newRun = new XWPFHyperlinkRun(hyperlink, ctr, (IRunBody) paragraph);
@@ -97,6 +105,22 @@ public class XWPFParagraphWrapper {
         }
 
         return null;
+    }
+    
+    private CTHyperlink insertNewHyperlink(int paramInt) {
+        // do not insert hyperlink directly #issue 331
+        // CTHyperlink hyperlink = paragraph.getCTP().insertNewHyperlink(rPos);
+
+        // the correct insert hyperlink as our run/irun list contains
+        // all runs
+        CTPImpl ctpImpl = (CTPImpl) paragraph.getCTP();
+        synchronized (ctpImpl.monitor()) {
+            // check_orphaned();
+            CTHyperlink localCTHyperlink = null;
+            localCTHyperlink = (CTHyperlink) ctpImpl.get_store().insert_element_user(RUN_QNAME_SET,
+                    HYPER_QNAME, paramInt);
+            return localCTHyperlink;
+        }
     }
 
     @SuppressWarnings("unchecked")
