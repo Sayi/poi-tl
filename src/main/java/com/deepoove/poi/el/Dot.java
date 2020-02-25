0458644b4e77f61c46281b01d781f7453254fac2
Sayi <adasai90@gmail.com>
2020-02-25 16:04:56 +0800

    introspector for el

--- src/main/java/com/deepoove/poi/el/Dot.java
+++ src/main/java/com/deepoove/poi/el/Dot.java
@@ -16,10 +16,14 @@
 package com.deepoove.poi.el;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Objects;
 import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.deepoove.poi.exception.ExpressionEvalException;
 
@@ -30,6 +34,7 @@ import com.deepoove.poi.exception.ExpressionEvalException;
  *
  */
 public class Dot {
+    private Logger logger = LoggerFactory.getLogger(Dot.class);
 
     private String el;
     private Dot target;
@@ -40,8 +45,9 @@ public class Dot {
 
     public Dot(String el) {
         Objects.requireNonNull(el, "EL cannot be null.");
-        if (!EL_PATTERN.matcher(el)
-                .matches()) { throw new ExpressionEvalException("Error EL fomart: " + el); }
+        if (!EL_PATTERN.matcher(el).matches()) {
+            throw new ExpressionEvalException("Error EL fomart: " + el);
+        }
 
         this.el = el;
         int dotIndex = el.lastIndexOf(".");
@@ -55,34 +61,43 @@ public class Dot {
 
     public Object eval(ELObject elObject) {
         if (elObject.cache.containsKey(el)) return elObject.cache.get(el);
-        Object result = null != target ? result = evalKey(target.eval(elObject))
-                : evalKey(elObject.model);
+        Object result = null != target ? result = evalKey(target.eval(elObject)) : evalKey(elObject.model);
         if (null != result) elObject.cache.put(el, result);
         return result;
     }
 
     private Object evalKey(Object obj) {
-        Objects.requireNonNull(obj,
-                "Cannot read value from null Prefix-Model, Prefix-Model EL: " + target);
+        Objects.requireNonNull(obj, "Cannot read value from null Prefix-Model, Prefix-Model EL: " + target);
         final Class<?> objClass = obj.getClass();
-        if (obj instanceof String || obj instanceof Number || obj instanceof java.util.Date
-                || obj instanceof Collection || objClass.isArray()
-                || objClass.isPrimitive()) { throw new ExpressionEvalException(
-                        "Prefix-Model must be JavaBean or Map, Prefix-Model EL: " + target
-                                + ", Prefix-Model type: " + objClass); }
+        if (obj instanceof String || obj instanceof Number || obj instanceof java.util.Date || obj instanceof Collection
+                || objClass.isArray() || objClass.isPrimitive()) {
+            throw new ExpressionEvalException("Prefix-Model must be JavaBean or Map, Prefix-Model EL: " + target
+                    + ", Prefix-Model type: " + objClass);
+        }
 
-        if (obj instanceof Map) { return ((Map<?, ?>) obj).get(key); }
+        if (obj instanceof Map) {
+            return ((Map<?, ?>) obj).get(key);
+        }
 
+        // introspector
+        Method readMethod = ReadMethodFinder.find(objClass, key);
+        if (null != readMethod) {
+            try {
+                return readMethod.invoke(obj);
+            } catch (Exception e) {
+                logger.info("Introspector {} fail: {}", key, e.getMessage());
+            }
+        }
+
+        // reflect
         Field field = FieldFinder.find(objClass, key);
         if (null == field) {
-            throw new ExpressionEvalException(
-                    "Cannot find the key:" + key + " from Prefix-Model EL:" + target);
+            throw new ExpressionEvalException("Cannot find the key:" + key + " from Prefix-Model EL:" + target);
         } else {
             try {
                 return field.get(obj);
             } catch (Exception e) {
-                throw new ExpressionEvalException(
-                        "Error read the property:" + key + " from " + objClass);
+                throw new ExpressionEvalException("Error read the property:" + key + " from " + objClass);
             }
         }
     }
---
+++ src/main/java/com/deepoove/poi/el/ReadMethodFinder.java
@@ -0,0 +1,24 @@
+package com.deepoove.poi.el;
+
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class ReadMethodFinder {
+
+    private static Logger logger = LoggerFactory.getLogger(ReadMethodFinder.class);
+
+    public static Method find(Class<?> objClass, String key) {
+        try {
+            PropertyDescriptor propDesc = new PropertyDescriptor(key, objClass);
+            return propDesc.getReadMethod();
+        } catch (IntrospectionException e1) {
+            logger.error("Fail introspector the property:" + key + " from " + objClass);
+        }
+        return null;
+    }
+
+}
