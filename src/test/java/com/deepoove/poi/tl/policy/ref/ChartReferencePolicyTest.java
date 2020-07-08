468003d0201bb41d74e7ba1c3b882ed0659e4aed
Sayi <adasai90@gmail.com>
2020-07-09 01:48:58 +0800

    面积图 testcase

--- src/test/java/com/deepoove/poi/tl/policy/ref/ChartReferencePolicyTest.java
+++ src/test/java/com/deepoove/poi/tl/policy/ref/ChartReferencePolicyTest.java
@@ -24,35 +24,39 @@ public class ChartReferencePolicyTest {
 
         Configure configure = Configure.newBuilder().bind("barChart", new MultiSeriesChartTemplateRenderPolicy())
                 .bind("VBarChart", new MultiSeriesChartTemplateRenderPolicy())
+                .bind("3dBarChart", new MultiSeriesChartTemplateRenderPolicy())
                 .bind("pieChart", new SingleSeriesChartTemplateRenderPolicy())
                 .bind("redarChart", new MultiSeriesChartTemplateRenderPolicy())
+                .bind("areaChart", new MultiSeriesChartTemplateRenderPolicy())
                 .bind("lineChart", new MultiSeriesChartTemplateRenderPolicy()).build();
 
         XWPFTemplate template = XWPFTemplate.compile("src/test/resources/template/reference_chart.docx", configure)
                 .render(new HashMap<String, Object>() {
                     {
-                        ChartMultiSeriesRenderData bar = new ChartMultiSeriesRenderData();
-                        bar.setChartTitle("ChartTitle");
-                        bar.setCategories(new String[] { "中文", "English", "日本語", "português", "中文", "English", "日本語",
+                        ChartMultiSeriesRenderData chart = new ChartMultiSeriesRenderData();
+                        chart.setChartTitle("ChartTitle");
+                        chart.setCategories(new String[] { "中文", "English", "日本語", "português", "中文", "English", "日本語",
                                 "português" });
                         List<SeriesRenderData> seriesRenderData = new ArrayList<SeriesRenderData>();
                         SeriesRenderData s0 = new SeriesRenderData();
                         s0.setName("countries");
-                        s0.setValues(new Double[] { 15.0, 6.0, 18.0, 31.0, 15.0, 6.0, 18.0, 31.0 });
+                        s0.setValues(new Double[] { 15.0, 6.0, 18.0, 231.0, 150.0, 6.0, 118.0, 31.0 });
                         seriesRenderData.add(s0);
                         SeriesRenderData s1 = new SeriesRenderData();
                         s1.setName("speakers");
-                        s1.setValues(new Double[] { 223.0, 119.0, 154.0, 442.0, 223.0, 119.0, 154.0, 442.0 });
+                        s1.setValues(new Double[] { 223.0, 119.0, 154.0, 142.0, 223.0, 119.0, 54.0, 42.0 });
                         seriesRenderData.add(s1);
                         SeriesRenderData s2 = new SeriesRenderData();
                         s2.setName("youngs");
-                        s2.setValues(new Double[] { 323.0, 119.0, 154.0, 442.0, 223.0, 119.0, 154.0, 442.0 });
+                        s2.setValues(new Double[] { 323.0, 89.0, 54.0, 42.0, 223.0, 119.0, 54.0, 442.0 });
                         seriesRenderData.add(s2);
-                        bar.setSeriesDatas(seriesRenderData);
-                        put("barChart", bar);
-                        put("VBarChart", bar);
-                        put("lineChart", bar);
-                        put("redarChart", bar);
+                        chart.setSeriesDatas(seriesRenderData);
+                        put("barChart", chart);
+                        put("VBarChart", chart);
+                        put("3dBarChart", chart);
+                        put("lineChart", chart);
+                        put("redarChart", chart);
+                        put("areaChart", chart);
 
                         ChartSingleSeriesRenderData pie = new ChartSingleSeriesRenderData();
                         pie.setChartTitle("ChartTitle");
