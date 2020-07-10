aa7e32b9f748ea3c321d940cf9f98f6899138696
Sayi <adasai90@gmail.com>
2020-07-10 17:27:58 +0800

    Release V1.8.0

--- src/test/java/com/deepoove/poi/tl/issue/Issue391.java
+++ src/test/java/com/deepoove/poi/tl/issue/Issue391.java
@@ -1,13 +1,18 @@
 package com.deepoove.poi.tl.issue;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.deepoove.poi.XWPFTemplate;
+import com.deepoove.poi.data.DocxRenderData;
 import com.deepoove.poi.xwpf.NiceXWPFDocument;
 
 public class Issue391 {
@@ -38,12 +43,22 @@ public class Issue391 {
         target = new NiceXWPFDocument(new FileInputStream(resource2));
 
         result = source.merge(target);
-        
+
         result.write(new FileOutputStream("out_issue_391.docx"));
 
         source.close();
         target.close();
         result.close();
+
+    }
+
+    @Test
+    public void testMerge() throws Exception {
+        Map<String, Object> datas = new HashMap<String, Object>();
+        datas.put("content", new DocxRenderData(new File("src/test/resources/issue/391_1MERGE.docx")));
+        datas.put("projectName", "project for test");
+
+        XWPFTemplate.compile("src/test/resources/issue/391_1.docx").render(datas);
 
     }
 }
