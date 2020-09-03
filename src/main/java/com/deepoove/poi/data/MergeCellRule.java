c8ae7590408a65a7cd8c6df368bc0a53028fea1b
Sayi <sayi90@163.com>
2020-09-03 16:41:14 +0800

    Add hashCode for MergeCellRule

--- src/main/java/com/deepoove/poi/data/MergeCellRule.java
+++ src/main/java/com/deepoove/poi/data/MergeCellRule.java
@@ -98,6 +98,15 @@ public class MergeCellRule implements Serializable {
         }
 
         @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + i;
+            result = prime * result + j;
+            return result;
+        }
+
+        @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
             if (obj == null) return false;
