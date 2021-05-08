6ff4807bbe7e553b24085b18d7bd00e30143dd11
Sayi <sayi90@163.com>
2021-05-08 12:22:00 +0800

    convert markdown to word docx

---
+++ src/main/java/com/deepoove/poi/plugin/markdown/MarkdownStyle.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2014-2020 Sayi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.deepoove.poi.plugin.markdown;
+
+import org.apache.poi.xwpf.usermodel.XWPFTable.XWPFBorderType;
+
+import com.deepoove.poi.data.style.BorderStyle;
+import com.deepoove.poi.data.style.CellStyle;
+import com.deepoove.poi.data.style.ParagraphStyle;
+import com.deepoove.poi.data.style.RowStyle;
+import com.deepoove.poi.data.style.Style;
+import com.deepoove.poi.plugin.highlight.HighlightStyle;
+
+/**
+ * @author Sayi
+ */
+public class MarkdownStyle {
+
+    private Style inlineCodeStyle;
+    private HighlightStyle highlightStyle;
+    private ParagraphStyle quoteStyle;
+    private BorderStyle tableBorderStyle;
+    private RowStyle tableHeaderStyle;
+
+    private boolean showHeaderNumber;
+    private String imagesDir = "";
+
+    public static MarkdownStyle newStyle() {
+        MarkdownStyle style = new MarkdownStyle();
+        style.setInlineCodeStyle(Style.builder().buildColor("f44336").buildFontFamily("Monaco").build());
+        style.setHighlightStyle(HighlightStyle.builder()
+//              .withShowLine(true)
+                .withTheme("zenburn")
+                .withFontFamily("Monaco")
+                .withFontSize(9.0f)
+                .build());
+        style.setQuoteStyle(ParagraphStyle.builder()
+                .withIndentLeftChars(0.5f)
+                .withLeftBorder(
+                        BorderStyle.builder().withColor("70ad47").withSize(48).withType(XWPFBorderType.SINGLE).build())
+                .build());
+
+        style.setTableBorderStyle(
+                BorderStyle.builder().withColor("BFBFBF").withSize(1).withType(XWPFBorderType.SINGLE).build());
+
+        // table header style
+        RowStyle headerStyle = new RowStyle();
+        CellStyle cellStyle = new CellStyle();
+        cellStyle.setBackgroundColor("5b9bd5");
+        Style textStyle = new Style();
+        textStyle.setColor("ffffff");
+        cellStyle.setDefaultParagraphStyle(ParagraphStyle.builder().withDefaultTextStyle(textStyle).build());
+        headerStyle.setDefaultCellStyle(cellStyle);
+        style.setTableHeaderStyle(headerStyle);
+        return style;
+    }
+
+    public Style getInlineCodeStyle() {
+        return inlineCodeStyle;
+    }
+
+    public void setInlineCodeStyle(Style inlineCodeStyle) {
+        this.inlineCodeStyle = inlineCodeStyle;
+    }
+
+    public HighlightStyle getHighlightStyle() {
+        return highlightStyle;
+    }
+
+    public void setHighlightStyle(HighlightStyle highlightStyle) {
+        this.highlightStyle = highlightStyle;
+    }
+
+    public ParagraphStyle getQuoteStyle() {
+        return quoteStyle;
+    }
+
+    public void setQuoteStyle(ParagraphStyle quoteStyle) {
+        this.quoteStyle = quoteStyle;
+    }
+
+    public BorderStyle getTableBorderStyle() {
+        return tableBorderStyle;
+    }
+
+    public void setTableBorderStyle(BorderStyle tableBorderStyle) {
+        this.tableBorderStyle = tableBorderStyle;
+    }
+
+    public RowStyle getTableHeaderStyle() {
+        return tableHeaderStyle;
+    }
+
+    public void setTableHeaderStyle(RowStyle tableHeaderStyle) {
+        this.tableHeaderStyle = tableHeaderStyle;
+    }
+
+    public boolean isShowHeaderNumber() {
+        return showHeaderNumber;
+    }
+
+    public void setShowHeaderNumber(boolean showHeaderNumber) {
+        this.showHeaderNumber = showHeaderNumber;
+    }
+
+    public String getImagesDir() {
+        return imagesDir;
+    }
+
+    public void setImagesDir(String imagesDir) {
+        this.imagesDir = imagesDir;
+    }
+
+}
---
+++ src/test/java/com/deepoove/poi/tl/plugin/TocTest.java
@@ -0,0 +1,79 @@
+package com.deepoove.poi.tl.plugin;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.XWPFStyles;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTP;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTSimpleField;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.STOnOff;
+
+import com.deepoove.poi.XWPFTemplate;
+import com.deepoove.poi.config.Configure;
+import com.deepoove.poi.policy.RenderPolicy;
+import com.deepoove.poi.template.ElementTemplate;
+import com.deepoove.poi.template.run.RunTemplate;
+
+public class TocTest {
+
+    private static final String HEADING1 = "1";
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        XWPFDocument doc = new XWPFDocument(new FileInputStream("toc.docx"));
+
+        XWPFStyles styles = doc.getStyles();
+        System.out.println(styles);
+
+        XWPFParagraph p = doc.createParagraph();
+        XWPFRun run = p.createRun();
+        run.setText("Heading 1");
+        p.getCTP().addNewPPr();
+        p.getCTP().getPPr().addNewPStyle();
+        p.setStyle(HEADING1);
+
+        XWPFParagraph tocPara = doc.createParagraph();
+        CTP ctP = tocPara.getCTP();
+        CTSimpleField toc = ctP.addNewFldSimple();
+        toc.setInstr("TOC \\h");
+        toc.setDirty(STOnOff.TRUE);
+
+        System.out.println(doc.isEnforcedUpdateFields());
+
+        // doc.enforceUpdateFields();
+
+        // doc.createTOC();
+
+        doc.write(new FileOutputStream("CreateWordBookmark.docx"));
+        doc.close();
+    }
+
+    public void testTOC() throws IOException {
+        Configure config = Configure.builder().bind("toc", new RenderPolicy() {
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void render(ElementTemplate eleTemplate, Object data, XWPFTemplate template) {
+                XWPFRun run = ((RunTemplate) eleTemplate).getRun();
+                run.setText("", 0);
+                XWPFParagraph tocPara = run.getParagraph();
+                CTP ctP = tocPara.getCTP();
+
+                CTSimpleField toc = ctP.addNewFldSimple();
+                toc.setInstr("TOC \\o");
+                toc.setDirty(STOnOff.TRUE);
+
+                // XWPFRun createRun = tocPara.createRun();
+                // createRun.addBreak(BreakType.PAGE);
+            }
+        }).build();
+        XWPFTemplate template = XWPFTemplate.compile("out_example_swagger.docx", config).render(new HashMap<>());
+        template.writeToFile("out_example_swagger_toc.docx");
+    }
+
+}
