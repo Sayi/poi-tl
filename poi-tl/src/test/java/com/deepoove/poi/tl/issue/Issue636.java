fed399ffcad6c9224b514a70ee851eb583d1871d
Sayi <sayi90@163.com>
2021-11-04 10:25:33 +0800

    w:ins bug

---
+++ poi-tl/src/test/java/com/deepoove/poi/tl/issue/Issue636.java
@@ -0,0 +1,14 @@
+package com.deepoove.poi.tl.issue;
+
+import org.junit.jupiter.api.Test;
+
+import com.deepoove.poi.XWPFTemplate;
+
+public class Issue636 {
+
+    @Test
+    public void test636() throws Exception {
+        XWPFTemplate.compile("src/test/resources/issue/636.docx");
+    }
+
+}
