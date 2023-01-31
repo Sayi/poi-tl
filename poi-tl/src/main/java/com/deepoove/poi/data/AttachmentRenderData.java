0ea3041a20cf18ca7f4e77bd9a578dffe3f8c026
Sayi <sayi90@163.com>
2023-01-31 11:25:57 +0800

    format code

--- poi-tl/src/main/java/com/deepoove/poi/data/AttachmentRenderData.java
+++ poi-tl/src/main/java/com/deepoove/poi/data/AttachmentRenderData.java
@@ -16,52 +16,52 @@
 package com.deepoove.poi.data;
 
 /**
- * attachment file:docx or xlsx
+ * attachment file
  *
  * @author Sayi
  */
 public abstract class AttachmentRenderData implements RenderData {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private AttachmentType fileType;
-	private PictureRenderData icon;
+    private AttachmentType fileType;
+    private PictureRenderData icon;
 
-	public abstract byte[] readAttachmentData();
+    public abstract byte[] readAttachmentData();
 
-	public AttachmentType getFileType() {
-		if (null != fileType) {
-			return fileType;
-		}
-		setFileType(detectFileType());
-		return fileType;
-	}
+    public AttachmentType getFileType() {
+        if (null != fileType) {
+            return fileType;
+        }
+        setFileType(detectFileType());
+        return fileType;
+    }
 
-	public void setFileType(AttachmentType fileType) {
-		this.fileType = fileType;
-	}
+    public void setFileType(AttachmentType fileType) {
+        this.fileType = fileType;
+    }
 
-	public PictureRenderData getIcon() {
-		return icon;
-	}
+    public PictureRenderData getIcon() {
+        return icon;
+    }
 
-	public void setIcon(PictureRenderData icon) {
-		this.icon = icon;
-	}
+    public void setIcon(PictureRenderData icon) {
+        this.icon = icon;
+    }
 
-	protected String getFileSrc() {
-		return null;
-	}
+    protected String getFileSrc() {
+        return null;
+    }
 
-	protected AttachmentType detectFileType() {
-		if (null == getFileSrc()) {
-			return null;
-		}
-		AttachmentType type = AttachmentType.suggestFileType(getFileSrc());
-		if (null == type) {
-			type = AttachmentType.suggestFileType(readAttachmentData());
-		}
-		return type;
-	}
+    protected AttachmentType detectFileType() {
+        if (null == getFileSrc()) {
+            return null;
+        }
+        AttachmentType type = AttachmentType.suggestFileType(getFileSrc());
+        if (null == type) {
+            type = AttachmentType.suggestFileType(readAttachmentData());
+        }
+        return type;
+    }
 
 }
